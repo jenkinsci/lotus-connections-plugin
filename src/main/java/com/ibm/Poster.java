9502275717acf899ec1cd1d0d1b222884ba30724
philrumble <prumble@au1.ibm.com>
2014-01-09 14:15:08 +0800

    Updated

--- src/main/java/com/ibm/Poster.java
+++ src/main/java/com/ibm/Poster.java
@@ -1,11 +1,7 @@
 package com.ibm;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.abdera.Abdera;
@@ -14,13 +10,6 @@ import org.apache.abdera.parser.Parser;
 import org.apache.abdera.protocol.Response.ResponseType;
 import org.apache.abdera.protocol.client.AbderaClient;
 import org.apache.abdera.protocol.client.ClientResponse;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
-import org.apache.commons.cli.PosixParser;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.lang.StringEscapeUtils;
 
@@ -97,5 +86,88 @@ public class Poster {
         // WTH?
         System.out.println("oh no!");
         }
+    }
+
+public void postForumTopic(String server,
+            String user,
+            String password,
+            String communityuuid,
+            String title,
+            String message)
+    {
+        if ((server != null) 
+                && (user != null) 
+                && (password != null) 
+                && (communityuuid != null) 
+                && (title != null) 
+                && (message != null))
+        {
+            fServer = server;
+            fUser = user;
+            fPasswd = password;
+            //fTitle = title;
+            fMessage = message;
+ 
+    
+	     // Initialize the Atom Client to update the status in Connections
+	        Abdera abdera = new Abdera();
+	        AbderaClient client = new AbderaClient(abdera);
+	        AbderaClient.registerTrustManager();
+	        try
+	        {
+	            client.addCredentials(fServer, null, null, new UsernamePasswordCredentials(fUser, fPasswd));
+	        }
+	        catch (URISyntaxException e)
+	        {
+	        e.printStackTrace();
+	        }
+	             
+	        String urlText =  fServer + "/communities/service/atom/community/forum/topics?communityUuid=" + communityuuid; 
+	
+	        Entry topic = abdera.newEntry();
+	        long now = System.currentTimeMillis();
+	        
+	
+	
+	        topic.addCategory("http://www.ibm.com/xmlns/prod/sn/type" ,"forum-topic", null);
+	
+	        topic.setTitle(title);
+	        topic.setContent(message);
+	        
+	
+	     // Send the new topic message to Connections
+	        
+	//        try
+	//        {
+	//            topic.writeTo(System.out);
+	//        }
+	//        catch (IOException e)
+	//        {
+	//            // TODO Auto-generated catch block
+	//            e.printStackTrace();
+	//        }
+	
+	        ClientResponse response = client.post(urlText, topic);
+	        
+	        if (response.getType() == ResponseType.SUCCESS)
+	        {
+	            // yipee
+	            System.out.println("woo hoo");
+	        }
+	        else if (response.getStatus() == 302) {
+	            
+	            String uri = response.getLocation().toString();
+	            System.out.println("re-directed to " + uri);
+	            response = client.post(uri, topic);
+	        }
+	        else
+	        {
+	            // WTH?
+	            System.out.println("oh no!");
+	            System.out.println(response.getStatus());
+	            System.out.println(response.getStatusText());
+	        }
+        }
+        
     }
 }
